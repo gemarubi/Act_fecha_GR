74d6101eb048b10f9584a0ac2f91032808ac74c6
Gema Rubio <gemarubio89@gmail.com>
2024-03-07 09:59:47 +0100

    Refactorizacion (renombrar fecha)

--- src/test/kotlin/FechaTest.kt
+++ src/test/kotlin/FechaTest.kt
@@ -1,9 +1,8 @@
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
-import kotlin.test.Test
 
-class FchTest {
+class FechaTest {
     @BeforeEach
     fun setUp() {
     }
@@ -14,27 +13,27 @@ class FchTest {
 
     @org.junit.jupiter.api.Test
     fun valida() {
-        val fecha = Fch(15, 13, 2023)
+        val fecha = Fecha(15, 13, 2023)
         assertTrue(fecha.valida())
     }
 
 
     @org.junit.jupiter.api.Test
     fun fechaInvalidaDia() {
-        val fecha = Fch(32, 5, 2022)
+        val fecha = Fecha(32, 5, 2022)
         kotlin.test.assertFalse(fecha.valida())
     }
 
     @org.junit.jupiter.api.Test
     fun fechaInvalidaMes() {
-        val fecha = Fch(12, 13, 2021)
+        val fecha = Fecha(12, 13, 2021)
         // Assert.assertFalse(fecha.valida())
         kotlin.test.assertFalse(fecha.valida())
     }
 
     @org.junit.jupiter.api.Test
     fun fechaInvalidaFebreroBisiesto() {
-        val fecha = Fch(29, 2, 2021)
+        val fecha = Fecha(29, 2, 2021)
         kotlin.test.assertFalse(fecha.valida())
         println("Fecha invalida")
         println(fecha.valida())
@@ -42,7 +41,7 @@ class FchTest {
 
     @org.junit.jupiter.api.Test
     fun fechaValidaFebreroBisiesto() {
-        val fecha = Fch(29, 2, 2024)
+        val fecha = Fecha(29, 2, 2024)
         assertTrue(fecha.valida())
         println("Fecha valida Bisiesto")
         println(fecha.valida())
